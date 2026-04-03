92bdab2d8650dbe39c4d3a8299de9981b297b03e
amartya13 <amartya13.haldar@gmail.com>
2026-04-03 11:15:21 -0400

    dynamic information with html script

--- first_app.py
+++ first_app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello this is the main page!"
-
-
-@app.route('/<name>')
-def user(name):
-    return f"Hello {name}!"
-
+    return render_template('index.html', content= "helloworld")
 
 if __name__ == '__main__':
     app.run()
