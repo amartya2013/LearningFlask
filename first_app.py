987bb7fe74d66be3c585debc97796eade6a4d779
amartya13 <amartya13.haldar@gmail.com>
2026-04-03 11:02:03 -0400

    Hello World Script for Flask

---
+++ first_app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello this is the main page!"
+
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name}!"
+
+
+if __name__ == '__main__':
+    app.run()
