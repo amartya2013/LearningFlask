249d74e8e9dc7b65aa5ea36e1003d492369e6ba5
amartya13 <amartya13.haldar@gmail.com>
2026-04-03 11:23:08 -0400

    Python inside html script

--- first_app.py
+++ first_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', content= "helloworld")
+    return render_template('index.html', content= "True")
 
 if __name__ == '__main__':
     app.run()
